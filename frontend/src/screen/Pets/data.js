85f9a06d769b0f20d804334bc2d190622c061ee3
ttphuongvi <trthphuongvigmail.com>
2021-12-11 14:09:37 +0700

    screen Pets tong quat

---
+++ frontend/src/screen/Pets/data.js
@@ -0,0 +1,46 @@
+import Scooby from './../../img/cho-corgi-1.jpg'
+const data = [
+    { 
+        src: Scooby,
+        name: 'Scooby',
+        type: 'Corgi',
+        age: '3',
+        characteristic: 'Thân dài, chân ngắn'
+    },
+    { 
+        src: Scooby,
+        name: 'Scooby',
+        type: 'Corgi',
+        age: '3',
+        characteristic: 'Thân dài, chân ngắn'
+    },
+    { 
+        src: Scooby,
+        name: 'Scooby',
+        type: 'Corgi',
+        age: '3',
+        characteristic: 'Thân dài, chân ngắn'
+    },
+    { 
+        src: Scooby,
+        name: 'Scooby',
+        type: 'Corgi',
+        age: '3',
+        characteristic: 'Thân dài, chân ngắn'
+    },
+    { 
+        src: Scooby,
+        name: 'Scooby',
+        type: 'Corgi',
+        age: '3',
+        characteristic: 'Thân dài, chân ngắn'
+    },
+    { 
+        src: Scooby,
+        name: 'Scooby',
+        type: 'Corgi',
+        age: '3',
+        characteristic: 'Thân dài, chân ngắn'
+    },
+];
+export default data;
--- frontend/src/screen/Pets/index.js
+++ frontend/src/screen/Pets/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import './styles.css'
+import data from './data';
 import Icon from '../../component/Icon/icon2';
+import { FaPaw } from "react-icons/fa";
+import {Link, Switch, Route} from "react-router-dom";
+import Home from './../Home/index'
 const Pets = () => {
     return (
         <div className="container-pets">
             <Icon title="Nhận nuôi thú cưng"/>
+            <div className="row">
+                {data.map((value) => (
+                    <div className="menber">
+                        <div className="img">
+                            <img src={value.src}></img>
+                        </div>
+                        <div className="content">
+                            <h4>{value.name}</h4>
+                            <ul className="info-pet">
+                                <li><FaPaw className="li-icon"/>Loại: {value.type}</li>
+                                <li><FaPaw className="li-icon"/>Tuổi: {value.age}</li>
+                                <li><FaPaw className="li-icon"/>Đặc điểm: {value.characteristic}</li>
+                            </ul>
+                        </div>
+                        <div className="btn-adopt"><Link className="adopt" to='/'>Nhận nuôi <FaPaw/></Link></div>
+                        
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
