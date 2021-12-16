101f1de767e0623b2c25cbda87f3838e98b83b42
ngdanhtrung <nhoxprokute03@gmail.com>
2021-12-16 14:44:33 +0700

    update requirements.txt

--- admin/src/components/layout/Sidenav.js
+++ admin/src/components/layout/Sidenav.js
@@ -89,7 +89,7 @@ function Sidenav({ color }) {
     <>
       <div className="brand">
         <img src={logo} alt="" />
-        <span>Muse Dashboard</span>
+        <span>Team ComChien</span>
       </div>
       <hr />
       <Menu theme="light" mode="inline">
