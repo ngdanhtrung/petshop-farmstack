a1210c9002928ffe97ca4d305d4628f82da3453f
chanhhanh <chanhhanh23100@gmail.com>
2021-12-17 14:42:32 +0700

    update products API

--- frontend/src/screen/Products/index.js
+++ frontend/src/screen/Products/index.js
@@ -5,16 +5,40 @@ import { connect } from "react-redux";
 import { loadCurrentItem, addCart } from "../../redux/actions";
 import { Link, Switch, Route, useRouteMatch } from "react-router-dom";
 import ProductDetails from "./ProductDetails/index";
-
+import axios from "axios";
 const Product = ({ product, addCart, loadCurrentItem }) => {
   let { path, url } = useRouteMatch();
+  const urlRequest = `${process.env.REACT_APP_API_KEY}items/addItem`;
+  const addSingleItem = async (product) => {
+    console.log(product);
+    await axios
+      .put(
+        urlRequest,
+        {
+          id: product._id,
+          name: product.name,
+          image: product.image,
+          quantity: 1,
+          value: product.value,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("access_token"),
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <Switch>
       <Route exact path={path}>
         <Link
           onClick={() => loadCurrentItem(product)}
-          to={`${url}/${product.id}`}
+          to={`${url}/${product._id}`}
           className='col-products link-products'
         >
           <img alt='a' src={product.image}></img>
@@ -23,7 +47,7 @@ const Product = ({ product, addCart, loadCurrentItem }) => {
           <div class='price'>{product.value} đ</div>
           <button
             className='btn-add-to-cart'
-            onClick={() => addCart(product.id)}
+            onClick={() => addSingleItem(product)}
           >
             Thêm vào giỏ hàng
           </button>
