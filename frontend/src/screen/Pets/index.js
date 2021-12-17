5783b0934a31507ed05bc2204df052e4da467159
chanhhanh <chanhhanh23100@gmail.com>
2021-12-17 18:21:53 +0700

    added search to web

--- frontend/src/screen/Pets/index.js
+++ frontend/src/screen/Pets/index.js
@@ -9,7 +9,13 @@ import ProfilePet from "../ProfilePet/index";
 const Pets = () => {
   let { path, url } = useRouteMatch();
   const [pets, setPets] = useState([]);
+  const [input, setInput] = useState("");
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value);
+  };
   const urlRequest = `${process.env.REACT_APP_API_KEY}products/getPets`;
+  const search = `${process.env.REACT_APP_API_KEY}products/searchPets`;
 
   const getPets = async () => {
     await axios
@@ -20,52 +26,71 @@ const Pets = () => {
       })
       .catch((error) => console.log(error));
   };
-  useEffect(getPets, []);
+  const searchPets = async () => {
+    await axios
+      .get(`${search}/${input}`)
+      .then((res) => {
+        setPets(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  };
+  useEffect(getPets, [input]);
+  useEffect(searchPets, [input]);
   return (
-    <div className='container-pets'>
-      <Switch>
-        <Route exact path={path}>
-          <>
-            <Icon title='Nhận nuôi thú cưng' />
-            <div className='row'>
-              {pets.map((value) => (
-                <div className='menber'>
-                  <div>
-                    <img className='img-pets' src={value.image}></img>
-                  </div>
-                  <div className='content'>
-                    <h4>{value.name}</h4>
-                    <ul className='info-pet'>
-                      <li>
-                        <FaPaw className='li-icon' />
-                        Loại: {value.extra.type}
-                      </li>
-                      <li>
-                        <FaPaw className='li-icon' />
-                        Tuổi: {value.extra.age}
-                      </li>
-                      <li>
-                        <FaPaw className='li-icon' />
-                        Đặc điểm: {value.extra.characteristic}
-                      </li>
-                    </ul>
-                  </div>
-                  <div className='btn-adopt'>
-                    <Link className='adopt' to={`/Pets/${value._id}`}>
-                      Nhận nuôi <FaPaw />
-                    </Link>
-                  </div>
-                  <hr></hr>
-                </div>
-              ))}
-            </div>
-          </>
-        </Route>
-        <Route path={`${path}/:id`}>
-          <ProfilePet />
-        </Route>
-      </Switch>
-    </div>
+    <>
+      <input value={input} onChange={onChangeHandler} />
+      <div className='container-pets'>
+        <Switch>
+          <Route exact path={path}>
+            <>
+              <Icon title='Nhận nuôi thú cưng' />
+              <div className='row'>
+                {pets ? (
+                  pets.map((value) => (
+                    <div className='menber'>
+                      <div>
+                        <img className='img-pets' src={value.image}></img>
+                      </div>
+                      <div className='content'>
+                        <h4>{value.name}</h4>
+                        <ul className='info-pet'>
+                          <li>
+                            <FaPaw className='li-icon' />
+                            Loại: {value.extra.type}
+                          </li>
+                          <li>
+                            <FaPaw className='li-icon' />
+                            Tuổi: {value.extra.age}
+                          </li>
+                          <li>
+                            <FaPaw className='li-icon' />
+                            Đặc điểm: {value.extra.characteristic}
+                          </li>
+                        </ul>
+                      </div>
+                      <div className='btn-adopt'>
+                        <Link className='adopt' to={`/Pets/${value._id}`}>
+                          Nhận nuôi <FaPaw />
+                        </Link>
+                      </div>
+
+                      <hr></hr>
+                    </div>
+                  ))
+                ) : (
+                  <div>ko tìm thấy gì cả</div>
+                )}
+              </div>
+            </>
+          </Route>
+          <Route path={`${path}/:id`}>
+            <ProfilePet />
+          </Route>
+        </Switch>
+      </div>
+      )
+    </>
   );
 };
 
--- frontend/src/screen/Products/Products.js
+++ frontend/src/screen/Products/Products.js
@@ -8,9 +8,16 @@ import { Link, Switch, Route, useRouteMatch } from "react-router-dom";
 import ProductDetails from "./ProductDetails/index";
 const Products = ({ products }) => {
   let { path, url } = useRouteMatch();
-
+  const [input, setInput] = useState("");
   const [items, setItems] = useState([]);
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value);
+  };
+
   const urlRequest = `${process.env.REACT_APP_API_KEY}products/getProducts`;
+  const search = `${process.env.REACT_APP_API_KEY}products/searchItems`;
+
   const getItems = async () => {
     await axios
       .get(urlRequest)
@@ -20,16 +27,33 @@ const Products = ({ products }) => {
       })
       .catch((error) => console.log(error));
   };
-  useEffect(getItems, []);
+  const searchItem = async () => {
+    await axios
+      .get(`${search}/${input}`)
+      .then((res) => {
+        setItems(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  };
+  useEffect(getItems, [input]);
+  useEffect(searchItem, [input]);
+
   return (
     <>
+      <input value={input} onChange={onChangeHandler} />
+
       <Switch>
         <Route exact path={path}>
           <Icon title='Các sản phẩm cho thú cưng của bạn' />
           <div className='row-products'>
-            {items.map((product) => (
-              <Product key={product._id} product={product} />
-            ))}
+            {items ? (
+              items.map((product) => (
+                <Product key={product._id} product={product} />
+              ))
+            ) : (
+              <div>ko</div>
+            )}
           </div>
         </Route>
         <Route path={`${path}/:id`}>
