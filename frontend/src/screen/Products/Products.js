98fc687766eda62b9ce8f45e8adfa0ae3ff3b349
chanhhanh <chanhhanh23100@gmail.com>
2021-12-17 14:26:47 +0700

    added products API to site

--- frontend/src/screen/Products/Products.js
+++ frontend/src/screen/Products/Products.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // Redux
 import { connect } from "react-redux";
 import Product from "./index";
-import Icon from "../../component/Icon/icon2"
-
+import Icon from "../../component/Icon/icon2";
+import axios from "axios";
 const Products = ({ products }) => {
+  const [items, setItems] = useState([]);
+  const urlRequest = `${process.env.REACT_APP_API_KEY}products/getProducts`;
+  const getItems = async () => {
+    await axios
+      .get(urlRequest)
+      .then((res) => {
+        setItems(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  };
+  useEffect(getItems, []);
   return (
     <>
-    <Icon title = "Các sản phẩm cho thú cưng của bạn"/>
-    <div className="row-products">
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
+      <Icon title='Các sản phẩm cho thú cưng của bạn' />
+      <div className='row-products'>
+        {items.map((product) => (
+          <Product key={product.id} product={product} />
         ))}
-    </div>
-        </>
+      </div>
+    </>
   );
 };
 
--- frontend/src/screen/Products/index.js
+++ frontend/src/screen/Products/index.js
@@ -1,38 +1,46 @@
-import React from 'react'
-import Icon from '../../component/Icon/icon2'
-import './styles.css'
-import {connect} from 'react-redux';
-import {loadCurrentItem,addCart} from '../../redux/actions'
-import {Link, Switch, Route, useRouteMatch} from 'react-router-dom'
-import ProductDetails from './ProductDetails/index'
+import React, { useEffect, useState } from "react";
+import Icon from "../../component/Icon/icon2";
+import "./styles.css";
+import { connect } from "react-redux";
+import { loadCurrentItem, addCart } from "../../redux/actions";
+import { Link, Switch, Route, useRouteMatch } from "react-router-dom";
+import ProductDetails from "./ProductDetails/index";
 
-const Product = ({product, addCart, loadCurrentItem}) => {
-    let { path, url } = useRouteMatch();
-    return (
-        <Switch>
-            <Route exact path={path}>
-                <Link onClick={() => loadCurrentItem(product)}
-                            to={`${url}/${product.id}`} className="col-products link-products">
-                    <img alt = "a"src={product.img}></img>
-                    <div className="type">{product.type}</div>
-                    <h3>{product.name}</h3>
-                    <div class="price">{product.price} đ</div>
-                    <button className="btn-add-to-cart" onClick={() => addCart(product.id)}>Thêm vào giỏ hàng</button>
-                    </Link>
-            </Route>
-            <Route path={`${path}/:id`}>
-                <ProductDetails/>
-            </Route>
-        </Switch>
-    )
-}
+const Product = ({ product, addCart, loadCurrentItem }) => {
+  let { path, url } = useRouteMatch();
 
+  return (
+    <Switch>
+      <Route exact path={path}>
+        <Link
+          onClick={() => loadCurrentItem(product)}
+          to={`${url}/${product.id}`}
+          className='col-products link-products'
+        >
+          <img alt='a' src={product.image}></img>
+          <div className='type'>{product.extra.type}</div>
+          <h3>{product.name}</h3>
+          <div class='price'>{product.value} đ</div>
+          <button
+            className='btn-add-to-cart'
+            onClick={() => addCart(product.id)}
+          >
+            Thêm vào giỏ hàng
+          </button>
+        </Link>
+      </Route>
+      <Route path={`${path}/:id`}>
+        <ProductDetails />
+      </Route>
+    </Switch>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-      addCart: (id) => dispatch(addCart(id)),
-      loadCurrentItem: (item) => dispatch(loadCurrentItem(item)),
-    };
+  return {
+    addCart: (id) => dispatch(addCart(id)),
+    loadCurrentItem: (item) => dispatch(loadCurrentItem(item)),
   };
-  //Because we don't use mapStateToProps the first param must be null
-  export default connect(null, mapDispatchToProps)(Product);
+};
+//Because we don't use mapStateToProps the first param must be null
+export default connect(null, mapDispatchToProps)(Product);
